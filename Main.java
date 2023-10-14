d5e69c402d9f9895fe7c8d99827d8055c88112a2
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-14 17:59:39 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package days6;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int nilai = 2;
+        int angka = 3;
+        System.out.println(angka=angka);
+        System.out.println(angka!=angka);
+        System.out.println(angka==angka);
+    }
+    
+}
